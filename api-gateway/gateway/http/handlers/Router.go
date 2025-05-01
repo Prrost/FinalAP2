9ad4a6901b5d8e558d59bbf9d34fd559c0e96ef5
Prrost <prrostik@gmail.com>
2025-05-01 20:19:26 +0500

    Cors fix

--- api-gateway/gateway/http/handlers/Router.go
+++ api-gateway/gateway/http/handlers/Router.go
@@ -3,11 +3,22 @@ package handlers
 import (
 	"api-gateway/config"
 	"api-gateway/gateway/grpc/clients"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func SetupRouter(cfg *config.Config, grpcClient *clients.Client) *gin.Engine {
 	router := gin.Default()
+
+	//Фикс проблемы CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{cfg.Front},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           100 * time.Hour,
+	}))
 
 	apiGroup := router.Group("/api")
 
